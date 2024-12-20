e9fb0738347ae4bcf36f4d1a94ca165b9f91a232
Richard <richard.infoo@gmail.com>
2024-12-20 13:13:41 -0300

    Update config.class.php
    Licenciamento update, tradução de legendas para pt_BR

--- inc/config.class.php
+++ inc/config.class.php
@@ -2,52 +2,50 @@
 
 /**
  * -------------------------------------------------------------------------
- * RoundRobin plugin for GLPI
+ * Plugin TicketBalance para GLPI
  * -------------------------------------------------------------------------
  *
- * LICENSE
+ * LICENÇA
  *
- * This file is part of RoundRobin GLPI Plugin.
+ * Este arquivo é parte do Plugin TicketBalance GLPI.
  *
- * RoundRobin is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
+ * TicketBalance é um software livre; você pode redistribuí-lo e/ou modificá-lo
+ * sob os termos da Licença Pública Geral GNU conforme publicada pela
+ * Free Software Foundation; seja na versão 3 da Licença, ou (a seu critério) qualquer versão posterior.
  *
- * RoundRobin is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * TicketBalance é distribuído na esperança de que seja útil,
+ * mas SEM QUALQUER GARANTIA; sem mesmo a garantia implícita de
+ * COMERCIALIZAÇÃO ou ADEQUAÇÃO A UM PROPÓSITO PARTICULAR. Consulte a Licença Pública Geral GNU para mais detalhes.
  *
- * You should have received a copy of the GNU General Public License
- * along with RoundRobin. If not, see <http://www.gnu.org/licenses/>.
+ * Você deve ter recebido uma cópia da Licença Pública Geral GNU
+ * junto com o TicketBalance. Se não, veja <http://www.gnu.org/licenses/>
  * -------------------------------------------------------------------------
- * @copyright Copyright (C) 2022 by initiativa s.r.l. - http://www.initiativa.it
+ * @copyright Copyright (C) 2024 - https://www.linkedin.com/in/richard-ti/
  * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
- * @link      https://github.com/initiativa/roundrobin
+ * @link      https://github.com/RPGMais/ticketbalance
  * -------------------------------------------------------------------------
  */
 class PluginRoundRobinConfig {
 
-    public static $PLUGIN_ROUNDROBIN_ENV = 'development';
-    public static $PLUGIN_ROUNDROBIN_NAME = 'Round Robin';
-    public static $PLUGIN_ROUNDROBIN_CODE = 'roundrobin';
-    public static $PLUGIN_ROUNDROBIN_VERSION = '1.0.1';
-    public static $PLUGIN_ROUNDROBIN_AUTHOR = '<a href="https://www.initiativa.it/glpi.php" target="_blank">initiativa s.r.l.</a>';
+    public static $PLUGIN_ROUNDROBIN_ENV = 'desenvolvimento';
+    public static $PLUGIN_ROUNDROBIN_NAME = 'Ticket Balance';
+    public static $PLUGIN_ROUNDROBIN_CODE = 'ticketbalance';
+    public static $PLUGIN_ROUNDROBIN_VERSION = '1.0.3';
+    public static $PLUGIN_ROUNDROBIN_AUTHOR = '<a href="https://www.linkedin.com/in/richard-ti/" target="_blank">Richard Loureiro</a>';
     public static $PLUGIN_ROUNDROBIN_LICENSE = 'GPLv3';
-    public static $PLUGIN_ROUNDROBIN_HOME_PAGE = 'https://github.com/initiativa/roundrobin/';
-    public static $PLUGIN_ROUNDROBIN_MIN_GLPI_VERSION = '9.5.5';
-    public static $PLUGIN_ROUNDROBIN_GLPI_VERSION_ERROR = "This plugin requires GLPI >= 9.5.5 and GLPI <= 10.0.99";
-    public static $PLUGIN_ROUNDROBIN_MAX_GLPI_VERSION = '10.0.99';
-    public static $PLUGIN_ROUNDROBIN_MAX_GLPI_VERSION_ERROR = 'This plugin requires ';
+    public static $PLUGIN_ROUNDROBIN_HOME_PAGE = 'https://www.initiativa.it/glpi.php';
+    public static $PLUGIN_ROUNDROBIN_MIN_GLPI_VERSION = '9.5.2';
+    public static $PLUGIN_ROUNDROBIN_GLPI_VERSION_ERROR = "Este plugin requer GLPI >= 9.5.2 ou GLPI <= 10.0.17";
+    public static $PLUGIN_ROUNDROBIN_MAX_GLPI_VERSION = '10.0.17';
+    public static $PLUGIN_ROUNDROBIN_MAX_GLPI_VERSION_ERROR = 'Este plugin requer ';
     public static $PLUGIN_ROUNDROBIN_MIN_PHP_VERSION = '7.3';
 
     public static function init() {
-        PluginRoundRobinLogger::addWarning(__METHOD__ . ' - defining hooks handlers');
+        PluginRoundRobinLogger::addWarning(__METHOD__ . ' - definindo manipuladores de hooks');
         global $PLUGIN_HOOKS;
         $PLUGIN_HOOKS['csrf_compliant'][self::$PLUGIN_ROUNDROBIN_CODE] = true;
         /**
-         * hooks declarations
+         * declarações de hooks
          */
         $PLUGIN_HOOKS['pre_item_add'][self::$PLUGIN_ROUNDROBIN_CODE] = [
             'Ticket' => 'plugin_roundrobin_hook_pre_item_add_handler'
@@ -97,9 +95,9 @@ class PluginRoundRobinConfig {
     public static function loadSources() {
         global $PLUGIN_HOOKS;
 
-        PluginRoundRobinLogger::addWarning(__METHOD__ . ' - loading sources...');
+        PluginRoundRobinLogger::addWarning(__METHOD__ . ' - carregando recursos...');
         /**
-         * add config section
+         * adicionar seção de configuração
          */
         $PLUGIN_HOOKS['config_page'][self::$PLUGIN_ROUNDROBIN_CODE] = 'front/config.form.php';
     }
@@ -108,15 +106,15 @@ class PluginRoundRobinConfig {
         global $PLUGIN_HOOKS;
 
         /**
-         * on uri page perform loading of necessary js code or styles
+         * na página URI, carregue o código js ou estilos necessários
          */
         if (is_array($uriArray) === false) {
-            throw new Exception("invalid URI structure, expected array");
+            throw new Exception("estrutura de URI inválida, esperado array");
         }
         foreach ($uriArray as $uri) {
             if (strpos(PluginRoundRobinRequest::getServerParam('REQUEST_URI'), $uri) !== false) {
                 $PLUGIN_HOOKS[$hook][self::$PLUGIN_ROUNDROBIN_CODE] = $sourceFile;
-                PluginRoundRobinLogger::addWarning(__METHOD__ . " - source $sourceFile loaded!");
+                PluginRoundRobinLogger::addWarning(__METHOD__ . " - recurso $sourceFile carregado!");
                 break;
             }
         }
