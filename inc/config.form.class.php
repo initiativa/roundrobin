0aeeff4faf4dd3b73aa0469e172da8ec0c1112a9
Richard <richard.infoo@gmail.com>
2024-12-20 12:24:29 -0300

    Update config.form.class.php
    Tradução para pt_BR

--- inc/config.form.class.php
+++ inc/config.form.class.php
@@ -2,27 +2,27 @@
 
 /**
  * -------------------------------------------------------------------------
- * RoundRobin plugin for GLPI
+ * Plugin TicketBalance para GLPI
  * -------------------------------------------------------------------------
  *
- * LICENSE
+ * LICENÇA
  *
- * This file is part of RoundRobin GLPI Plugin.
+ * Este arquivo é parte do plugin RoundRobin GLPI.
  *
- * RoundRobin is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
+ * RoundRobin é um software livre; você pode redistribuí-lo e/ou modificá-lo
+ * sob os termos da Licença Pública Geral GNU conforme publicada pela
+ * Free Software Foundation; seja na versão 3 da Licença, ou (a seu critério) 
+ * qualquer versão posterior.
  *
- * RoundRobin is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * RoundRobin é distribuído na esperança de que seja útil,
+ * mas SEM QUALQUER GARANTIA; sem mesmo a garantia implícita de
+ * COMERCIALIZAÇÃO ou ADEQUAÇÃO A UM PROPÓSITO PARTICULAR. Consulte
+ * a Licença Pública Geral GNU para mais detalhes.
  *
- * You should have received a copy of the GNU General Public License
- * along with RoundRobin. If not, see <http://www.gnu.org/licenses/>.
+ * Você deve ter recebido uma cópia da Licença Pública Geral GNU
+ * junto com o RoundRobin. Se não, veja <http://www.gnu.org/licenses/>.
  * -------------------------------------------------------------------------
- * @copyright Copyright (C) 2022 by initiativa s.r.l. - http://www.initiativa.it
+ * @copyright Copyright (C) 2022 por iniciativa s.r.l. - http://www.initiativa.it
  * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
  * @link      https://github.com/initiativa/roundrobin
  * -------------------------------------------------------------------------
@@ -34,14 +34,14 @@ require_once 'RRAssignmentsEntity.class.php';
 class PluginRoundRobinSettings extends CommonDBTM {
 
     public function __construct() {
-        PluginRoundRobinLogger::addWarning(__METHOD__ . ' - constructor called');
+        PluginRoundRobinLogger::addWarning(__METHOD__ . ' - construtor chamado');
     }
 
     public function renderTitle() {
         $injectHTML = <<< EOT
                 <p>
                     <div align='center'>
-                        <h1>RoundRobin Settings</h1>
+                        <h1>Configurações do RoundRobin</h1>
                     </div>
                 </p>
 EOT;
@@ -52,16 +52,16 @@ EOT;
         global $CFG_GLPI, $DB;
 
         if (self::checkCentralInterface()) {
-            PluginRoundRobinLogger::addWarning(__METHOD__ . ' - display contents');
+            PluginRoundRobinLogger::addWarning(__METHOD__ . ' - exibir conteúdo');
             self::displayContent();
         } else {
-            echo "<div align='center'><br><img src='" . $CFG_GLPI['root_doc'] . "/pics/warning.png'><br>" . __("Access denied") . "</div>";
+            echo "<div align='center'><br><img src='" . $CFG_GLPI['root_doc'] . "/pics/warning.png'><br>" . __("Acesso negado") . "</div>";
         }
     }
 
     public static function checkCentralInterface() {
         $currentInterface = Session::getCurrentInterface();
-        PluginRoundRobinLogger::addWarning(__METHOD__ . ' - current interface: ' . $currentInterface);
+        PluginRoundRobinLogger::addWarning(__METHOD__ . ' - interface atual: ' . $currentInterface);
         return $currentInterface === 'central';
     }
 
@@ -70,41 +70,41 @@ EOT;
         echo "<form name='settingsForm' action='config.form.php' method='post' enctype='multipart/form-data'>";
         echo Html::hidden('_glpi_csrf_token', ['value' => Session::getNewCSRFToken()]);
         echo "<table class='tab_cadre_fixe'>";
-        echo "<tr><th colspan='4'>" . "Enable Group Round Robin Ticket Assignment for each ITILCategory" . "</th></tr>";
+        echo "<tr><th colspan='4'>" . "Ativar atribuição de tickets Round Robin para cada ITILCategory" . "</th></tr>";
         echo "<tr><th colspan='4'>" . "<hr />" . "</th></tr>";
 
         /**
-         * option row
+         * linha de opção
          */
         echo "<tr><th colspan='4'>";
-        echo "Assign also to the original Group: &nbsp;&nbsp; <input type='radio' name='auto_assign_group' value='1'";
+        echo "Atribuir também ao Grupo original: &nbsp;&nbsp; <input type='radio' name='auto_assign_group' value='1'";
         $auto_assign_group = self::getAutoAssignGoup();
         if ($auto_assign_group) {
             echo "checked='checked'";
         }
-        echo "> Yes&nbsp;&nbsp;";
+        echo "> Sim&nbsp;&nbsp;";
         echo "<input type='radio' name='auto_assign_group' value='0'";
         if (!$auto_assign_group) {
             echo "checked='checked'";
         }
-        echo "> No";
+        echo "> Não";
         echo "</th></tr>";
 
         /**
-         * assignments rows
+         * linhas de atribuições
          */
         echo "<tr><th colspan='4'>" . "<hr />" . "</th></tr>";
-        echo "<tr><th>ITILCategory</th><th>Group</th><th>Members #</th><th>Setting</th></tr>";
+        echo "<tr><th>ITILCategory</th><th>Grupo</th><th>Número de Membros</th><th>Configuração</th></tr>";
 
         /**
-         * render each row for profile and settings
+         * renderizar cada linha para perfil e configurações
          */
         foreach (self::getSettings() as $row) {
             $id = $row['id'];
             $itilcategories_id = $row['itilcategories_id'];
             $category_name = $row['category_name'];
-            $group_name = isset($row['group_name']) ? $row['group_name'] : "<em>No group assigned</em>";
-            $num_group_members = isset($row['group_name']) ? $row['num_group_members'] : "<em>N/A</em>";
+            $group_name = isset($row['group_name']) ? $row['group_name'] : "<em>Nenhum grupo atribuído</em>";
+            $num_group_members = isset($row['group_name']) ? $row['num_group_members'] : "<em/N/A</em>";
             $is_active = $row['is_active'];
 
             echo "<tr><td>$category_name</td><td>$group_name</td><td>$num_group_members</td>";
@@ -114,18 +114,18 @@ EOT;
             if ($is_active) {
                 echo "checked='checked'";
             }
-            echo "> Enabled&nbsp;&nbsp;";
+            echo "> Ativado&nbsp;&nbsp;";
             echo "<input type='radio' name='is_active_$id' value='0'";
             if (!$is_active) {
                 echo "checked='checked'";
             }
-            echo "> Disabled</td></tr>";
+            echo "> Desativado</td></tr>";
         }
         /**
-         * controls
+         * controles
          */
         echo "<tr><td colspan='4'><hr/></td></tr>";
-        echo "<tr><td colspan='3'>&nbsp;<td><input type='submit' name='save' class='submit' value=" . __('Save') . ">&nbsp;&nbsp;<input type='submit' class='submit' name='cancel' value=" . __('Cancel') . "></td></tr>";
+        echo "<tr><td colspan='3'>&nbsp;<td><input type='submit' name='save' class='submit' value=" . __('Salvar') . ">&nbsp;&nbsp;<input type='submit' class='submit' name='cancel' value=" . __('Cancelar') . "></td></tr>";
         echo "</table>";
     }
 
@@ -144,12 +144,12 @@ EOT;
         $rrAssignmentsEntity = new PluginRoundRobinRRAssignmentsEntity();
 
         /**
-         * save option(s)
+         * salvar opção(ões)
          */
         $rrAssignmentsEntity->updateAutoAssignGroup($_POST['auto_assign_group']);
 
         /**
-         * save all assignments
+         * salvar todas as atribuições
          */
         foreach (self::getSettings() as $row) {
             $itilCategoryId = $_POST["itilcategories_id_{$row['id']}"];
