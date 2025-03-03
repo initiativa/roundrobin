6d278483591c536086574767d5cac80ab7cc3c07
tomasz <gitmail@creatively.dev>
2025-03-03 16:43:58 +0100

    typing fix

--- inc/TicketHookHandler.class.php
+++ inc/TicketHookHandler.class.php
@@ -17,7 +17,7 @@
  * RoundRobin is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Publi6789c License for more details.
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with RoundRobin. If not, see <http://www.gnu.org/licenses/>.
